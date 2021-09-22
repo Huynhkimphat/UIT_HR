0468aad64af585564b54bf06919f9e222bb9384a
HKP <kimphat.devcs@gmail.com>
2021-09-22 09:01:54 +0700

    feature/AccountMapper: Init EmployeeAccountMapper

---
+++ HR_UIT.Web/Serialization/EmployeeAccountMapper.cs
@@ -0,0 +1,46 @@
+﻿using HR_UIT.Data.Models;
+using HR_UIT.Web.ViewModels;
+
+namespace HR_UIT.Web.Serialization
+{
+    public class EmployeeAccountMapper
+    {
+        /// <summary>
+        ///     Maps a EmployeeAccount Data Model to EmployeeAccount View Model
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public static EmployeeAccountModel
+            MapEmployeeAccount(EmployeeAccount account)
+        {
+            return new EmployeeAccountModel
+            {
+                Id = account.Id,
+                Email = account.Email,
+                Password = account.Password,
+                CreatedOn = account.CreatedOn,
+                UpdatedOn = account.UpdatedOn,
+                IsArchived = account.IsArchived,
+            };
+        }
+        
+        
+        /// <summary>
+        ///     Maps a EmployeeAccount View Model to EmployeeAccount Data Model
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public static EmployeeAccount
+            MapEmployeeAccount(EmployeeAccountModel account)
+        {
+            return new EmployeeAccount
+            {
+                Email = account.Email,
+                Password = account.Password,
+                CreatedOn = account.CreatedOn,
+                UpdatedOn = account.UpdatedOn,
+                IsArchived = account.IsArchived,
+            };
+        }
+    }
+}

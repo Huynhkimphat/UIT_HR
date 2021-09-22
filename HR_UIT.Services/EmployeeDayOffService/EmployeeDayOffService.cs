58a3c1701791ecc113fde27c2e3f505932964430
thainta <thai1612200110a@gmail.com>
2021-09-22 15:48:36 +0700

    Feature/DayOffService: Update Interface

--- HR_UIT.Services/EmployeeDayOffService/EmployeeDayOffService.cs
+++ HR_UIT.Services/EmployeeDayOffService/EmployeeDayOffService.cs
@@ -1,4 +1,8 @@
-﻿namespace HR_UIT.Services.EmployeeDayOffService
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using HR_UIT.Data;
+namespace HR_UIT.Services.EmployeeDayOffService
 {
     public class EmployeeDayOffService
     {
--- HR_UIT.Services/EmployeeDayOffService/IEmployeeDayOffService.cs
+++ HR_UIT.Services/EmployeeDayOffService/IEmployeeDayOffService.cs
@@ -4,20 +4,23 @@ namespace HR_UIT.Services.EmployeeDayOffService
 {
     public interface IEmployeeDayOffServices
     {
-        ServiceResponse<Data.Models.EmployeeDayOff> 
+        ServiceResponse<Data.Models.EmployeeDayOff>
             CreateNewEmployeeDayOff(
                 Data.Models.EmployeeDayOff employeeDayOff
             );
-        
-        ServiceResponse<Data.Models.EmployeeDayOff> 
+
+        ServiceResponse<Data.Models.EmployeeDayOff>
             UpdateEmployeeDayOff(
                 Data.Models.EmployeeDayOff employeeDayOff
             );
-        
+
         List<Data.Models.EmployeeDayOff> GetAllEmployeeDayOff();
 
         Data.Models.EmployeeDayOff GetEmployeeDayOffById(int id);
-        
+
         ServiceResponse<bool> DeleteEmployeeDayOff(int id);
+
+        ServiceResponse<Data.Models.EmployeeDayOff> RecoverEmployeeDayOff(int id);
+
     }
 }

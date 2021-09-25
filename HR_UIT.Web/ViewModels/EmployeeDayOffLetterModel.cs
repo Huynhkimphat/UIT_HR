03658ed029ab6d00949947779798a4bc5c9aeed3
thainta <thai1612200110a@gmail.com>
2021-09-25 11:15:43 +0700

    Feature/DayOffLetterModel: Create

---
+++ HR_UIT.Web/ViewModels/EmployeeDayOffLetterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HR_UIT.Web.ViewModels
+{
+    public class EmployeeDayOffLetterModel
+    {
+        public int Id { get; set; }
+
+        public DateTime FromDateTime { get; set; }
+
+        public DateTime ToDateTime { get; set; }
+
+        public string Reason { get; set; }
+        
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime UpdatedOn { get; set; }
+        
+        public bool IsArchived { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public int DayOffCounting { get; set; }
+    }
+}

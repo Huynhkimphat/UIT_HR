e1ad890ef6c9942a9f82620faaa9853562fefcb7
thainta <thai1612200110a@gmail.com>
2021-09-25 18:59:08 +0700

    Feature/DayOffMapper: Create

---
+++ HR_UIT.Web/Serialization/EmployeeDayOffMapper.cs
@@ -0,0 +1,14 @@
+﻿using HR_UIT.Data.Models;
+using HR_UIT.Web.ViewModels;
+
+namespace HR_UIT.Web.Serialization
+{
+    public static class EmployeeDayOffMapper
+    {
+        public static EmployeeDayOffModel
+            EmployeeDayOff(EmployeeDayOff dayOff)
+        {
+            return new EmployeeDayOffMapper
+        }
+    }
+}

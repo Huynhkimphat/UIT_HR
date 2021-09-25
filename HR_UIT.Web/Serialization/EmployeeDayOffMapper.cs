011205dbc42e393a69b6a5c6ea085f1bae144cdd
thainta <thai1612200110a@gmail.com>
2021-09-25 19:10:26 +0700

    Feature/DayOffMapper: Update

--- HR_UIT.Web/Serialization/EmployeeDayOffMapper.cs
+++ HR_UIT.Web/Serialization/EmployeeDayOffMapper.cs
@@ -5,10 +5,39 @@ namespace HR_UIT.Web.Serialization
 {
     public static class EmployeeDayOffMapper
     {
+        /// <summary>
+        /// Maps a EmployeeDayOff Data Model to EmployeeDayOff View Model
+        /// </summary>
+        /// <param name="dayOff"></param>
+        /// <returns></returns>
         public static EmployeeDayOffModel
             EmployeeDayOff(EmployeeDayOff dayOff)
         {
-            return new EmployeeDayOffMapper
+            return new EmployeeDayOffModel
+            {
+                Id = dayOff.Id,
+                DayOffAmount = dayOff.DayOffAmount,
+                CreatedOn = dayOff.CreatedOn,
+                UpdatedOn = dayOff.UpdatedOn,
+                IsArchived = dayOff.IsArchived
+            };
+        }
+        
+        /// <summary>
+        /// Maps a EmployeeDayOff View Model to EmployeeDayOff Data Model
+        /// </summary>
+        /// <param name="dayOff"></param>
+        /// <returns></returns>
+        public static EmployeeDayOff
+            EmployeeDayOff(EmployeeDayOffModel dayOff)
+        {
+            return new EmployeeDayOff
+            {
+                DayOffAmount = dayOff.DayOffAmount,
+                CreatedOn = dayOff.CreatedOn,
+                UpdatedOn = dayOff.UpdatedOn,
+                IsArchived = dayOff.IsArchived
+            };
         }
     }
 }

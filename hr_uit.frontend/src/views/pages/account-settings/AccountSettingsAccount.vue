d1eb5744ba15e534df10433833eec43dc195f3cb
Phat Huynh <kimphat.devcs@gmail.com>
2021-11-03 08:52:20 +0700

    Update Bug Employee Flow

--- hr_uit.frontend/src/views/pages/account-settings/AccountSettingsAccount.vue
+++ hr_uit.frontend/src/views/pages/account-settings/AccountSettingsAccount.vue
@@ -1,103 +1,44 @@
 <template>
-  <v-card
-    flat
-    class="pa-3 mt-2"
-  >
+  <v-card flat class="pa-3 mt-2">
     <v-card-text class="d-flex">
-      <v-avatar
-        rounded
-        size="120"
-        class="me-6"
-      >
+      <v-avatar rounded size="120" class="me-6">
         <v-img :src="accountDataLocale.avatarImg"></v-img>
       </v-avatar>
 
       <!-- upload photo -->
       <div>
-        <v-btn
-          color="primary"
-          class="me-3 mt-5"
-          @click="$refs.refInputEl.click()"
-        >
+        <v-btn color="primary" class="me-3 mt-5" @click="$refs.refInputEl.click()">
           <v-icon class="d-sm-none">
             {{ icons.mdiCloudUploadOutline }}
           </v-icon>
           <span class="d-none d-sm-block">Upload new photo</span>
         </v-btn>
 
-        <input
-          ref="refInputEl"
-          type="file"
-          accept=".jpeg,.png,.jpg,GIF"
-          :hidden="true"
-        />
+        <input ref="refInputEl" type="file" accept=".jpeg,.png,.jpg,GIF" :hidden="true" />
 
-        <v-btn
-          color="error"
-          outlined
-          class="mt-5"
-        >
-          Reset
-        </v-btn>
-        <p class="text-sm mt-5">
-          Allowed JPG, GIF or PNG. Max size of 800K
-        </p>
+        <v-btn color="error" outlined class="mt-5"> Reset </v-btn>
+        <p class="text-sm mt-5">Allowed JPG, GIF or PNG. Max size of 800K</p>
       </div>
     </v-card-text>
 
     <v-card-text>
       <v-form class="multi-col-validation mt-6">
         <v-row>
-          <v-col
-            md="6"
-            cols="12"
-          >
-            <v-text-field
-              v-model="currentDataRecord.firstName"
-              label="First Name"
-              dense
-              outlined
-            ></v-text-field>
+          <v-col md="6" cols="12">
+            <v-text-field v-model="currentDataRecord.firstName" label="First Name" dense outlined></v-text-field>
           </v-col>
-          <v-col
-            md="6"
-            cols="12"
-          >
-            <v-text-field
-              v-model="currentDataRecord.lastName"
-              label="Last Name"
-              dense
-              outlined
-            ></v-text-field>
+          <v-col md="6" cols="12">
+            <v-text-field v-model="currentDataRecord.lastName" label="Last Name" dense outlined></v-text-field>
           </v-col>
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="currentDataRecord.primaryAccount.email"
-              label="Email"
-              dense
-              outlined
-            ></v-text-field>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="currentDataRecord.primaryAccount.email" label="Email" dense outlined></v-text-field>
           </v-col>
 
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="currentRole"
-              dense
-              label="Role"
-              outlined
-            ></v-text-field>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="currentRole" dense label="Role" outlined></v-text-field>
           </v-col>
 
-          <v-col
-            cols="12"
-            md="6"
-          >
+          <v-col cols="12" md="6">
             <v-select
               v-model="currentDataRecord.isArchived"
               dense
@@ -111,24 +52,15 @@
 
           <!-- alert -->
           <v-col cols="12">
-            <v-alert
-              color="warning"
-              text
-              class="mb-0"
-            >
+            <v-alert color="warning" text class="mb-0">
               <div class="d-flex align-start">
                 <v-icon color="warning">
                   {{ icons.mdiAlertOutline }}
                 </v-icon>
 
                 <div class="ms-3">
-                  <p class="text-base font-weight-medium mb-1">
-                    Your email is not confirmed. Please check your inbox.
-                  </p>
-                  <a
-                    href="javascript:void(0)"
-                    class="text-decoration-none warning--text"
-                  >
+                  <p class="text-base font-weight-medium mb-1">Your email is not confirmed. Please check your inbox.</p>
+                  <a href="javascript:void(0)" class="text-decoration-none warning--text">
                     <span class="text-sm">Resend Confirmation</span>
                   </a>
                 </div>
@@ -137,21 +69,8 @@
           </v-col>
 
           <v-col cols="12">
-            <v-btn
-              color="primary"
-              class="me-3 mt-4"
-            >
-              Save changes
-            </v-btn>
-            <v-btn
-              color="secondary"
-              outlined
-              class="mt-4"
-              type="reset"
-              @click.prevent="resetForm"
-            >
-              Cancel
-            </v-btn>
+            <v-btn color="primary" class="me-3 mt-4"> Save changes </v-btn>
+            <v-btn color="secondary" outlined class="mt-4" type="reset" @click.prevent="resetForm"> Cancel </v-btn>
           </v-col>
         </v-row>
       </v-form>
@@ -204,7 +123,16 @@ export default {
     }
   },
   computed: {
+    ...mapGetters('employeeStore', ['getEmployee']),
     ...mapGetters(['getRole']),
+  },
+  watch: {
+    getEmployee(value) {
+      this.currentDataRecord = value
+    },
+    getRole(value) {
+      this.currentRole = value
+    },
   },
 }
 </script>

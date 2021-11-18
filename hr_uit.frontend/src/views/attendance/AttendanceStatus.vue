9a1bea0e79e120ab6fde858b5e0b4c85b9c6bb9c
DyHon <lehoang8d@gmail.com>
2021-11-18 21:06:30 +0700

    Add 1 line code

--- hr_uit.frontend/src/views/attendance/AttendanceStatus.vue
+++ hr_uit.frontend/src/views/attendance/AttendanceStatus.vue
@@ -65,6 +65,7 @@ export default {
     if (this.getEmployee.employeeAttendances.at(0).isProgressing === true) {
       this.isCheckIn = true
     }
+    console.log(this.getEmployee.employeeAttendances.at(0).id)
   },
   methods: {
     ...mapMutations('attendanceStore', ['resetState']),
@@ -96,7 +97,7 @@ export default {
         {
           attendance: this.newAttendance,
           token: this.$store.state.token,
-          attendanceId: this.$store.state.attendanceStore.createResponse.data.id,
+          attendanceId: this.getEmployee.employeeAttendances.at(0).id,
         }).then(
         this.resetState(),
         this.resetNewAttendance(),
